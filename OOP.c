2715774e0b658e35bb026b450e22c3d59139df43
Shamil <54128722+ShamilSE@users.noreply.github.com>
2021-04-14 13:36:40 +0300

    Create OOP.c

---
+++ OOP.c
@@ -0,0 +1,41 @@
+array_minmax_t *array_minmax_create(unsigned int length, int *input_array) {
+  array_minmax_t  *array;
+  array = malloc(sizeof(array_minmax_t));
+  array->int_array = input_array;
+  array->length = length;
+  return array;
+}
+
+void array_minmax_add(array_minmax_t *array, int number) {
+  array->int_array[array->length] = number;
+  array->length++;
+  printf("array->length: %u\nnumber to add is %d\n", array->length, number);
+}
+
+int array_minmax_get_min(array_minmax_t *array) {
+  int min;
+  
+  min = array->int_array[0];
+  for (unsigned int i = 0; i < array->length; i++) {
+    if (array->int_array[i] < min) {
+      min = array->int_array[i];
+    }
+  }
+  return min;
+}
+
+int array_minmax_get_max(array_minmax_t *array) {
+  int max;
+  
+  max = array->int_array[0];
+  for (unsigned int i = 0; i < array->length; i++) {
+    if (array->int_array[i] > max) {
+      max = array->int_array[i];
+    }
+  }
+  return max;
+}
+
+void array_minmax_destroy(array_minmax_t *array) {
+  free(array);
+}
