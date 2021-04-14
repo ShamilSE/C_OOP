b101b6e21bf53a4a3bd307c0299afaf6144ba264
Shamil <54128722+ShamilSE@users.noreply.github.com>
2021-04-14 13:37:41 +0300

    Create OOP.h

---
+++ OOP.h
@@ -0,0 +1,12 @@
+#ifndef OOP_H
+# define OOP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _array_minmax_t {
+  int          *int_array;
+  unsigned int length;
+}              array_minmax_t;
+
+#endif
